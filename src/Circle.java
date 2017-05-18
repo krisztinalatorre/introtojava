5c0c2730a26a377ebc0eb9f54e89c287a71886dd
Krisztina La-Torre <krisztina.la-torre@liferay.com>
2017-05-18 23:19:10 +0200

    problem set til flower

---
+++ src/Circle.java
@@ -0,0 +1,22 @@
+import java.awt.Rectangle;
+
+import javax.sound.sampled.Line;
+
+public class Circle
+{
+    public static void main(String[] args)
+    {
+        Rectangle face = new Rectangle (20, 30, 100, 40);
+//        face.draw();
+//        Line line1 = new Line(20, 30, 50, 10);
+//        line1.draw();
+//        Line line2 = new Line(120, 30, 150, 10);
+//        line2.draw();
+//        Line line3 = new Line(50, 10, 150, 10);
+//        line3.draw();
+//        Line line4 = new Line(150, 10, 150, 50);
+//        line4.draw();
+//        Line line5 = new Line(120, 70, 150, 50);
+//        line5.draw();
+    }
+}
